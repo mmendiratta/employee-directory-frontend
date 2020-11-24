a7a062b8ba2a39b1a54d869cd2d5e152e0e97815
ManavMendiratta <Manav.Mendiratta@target.com>
2020-11-24 16:20:34 -0600

    added login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./components/Homepage.js";
+import Login from "./components/Login.js";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
-  return <Homepage></Homepage>;
+  return (
+    <HashRouter basename="/">
+      <div>
+        <Route path="/login" component={LoginPage} />
+        <Route exact path="/home" component={Home} />
+      </div>
+    </HashRouter>
+  );
 }
+
+const Home = () => <Homepage></Homepage>;
+const LoginPage = () => <Login></Login>;
 
 export default App;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import TextField from '@material-ui/core/TextField';
 import TableDirectory from './Table';
 import FormDirectory from './Form';
 
